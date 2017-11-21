74b42e1818e254067d8084cd68717bec090b5541
baki504 <baki504@msn.com>
2017-11-21 22:51:33 +0900

    lomnokのアノテーション見直し

--- src/main/java/a/b/c/domain/testscript/TestCase.java
+++ src/main/java/a/b/c/domain/testscript/TestCase.java
@@ -1,12 +1,10 @@
 package a.b.c.domain.testscript;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
-@Setter
-@Getter
 public class TestCase {
 
 	private String name;
--- src/main/java/a/b/c/domain/testscript/TestScript.java
+++ src/main/java/a/b/c/domain/testscript/TestScript.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
-@Setter
-@Getter
 public class TestScript {
 
 	private String filePath;
