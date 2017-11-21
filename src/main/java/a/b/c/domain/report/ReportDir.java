9b7b7fad27d4e2e1eca7a831bdad607d8b2b8ce1
baki504 <baki504@msn.com>
2017-11-21 22:44:05 +0900

    bootstrapをCDN経由で利用する方式に修正

--- src/main/java/a/b/c/domain/report/ReportDir.java
+++ src/main/java/a/b/c/domain/report/ReportDir.java
@@ -1,5 +1,6 @@
 package a.b.c.domain.report;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +13,7 @@ public class ReportDir {
 
 	private static File dir;
 
-	private static final String[] reportResouces = { "css/style.css", "/css/bootstrap.min.css" };
+	private static final String REPORT_RESOURCE = "css/style.css";
 
 	static {
 		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
@@ -21,11 +22,9 @@ public class ReportDir {
 		dir = new File(reportDirName);
 
 		try {
-			for (int i = 0; i < reportResouces.length; i++) {
-				InputStream srcStream = ClassLoader.getSystemResourceAsStream("report/" + reportResouces[i]);
-				File destFile = new File(dir, reportResouces[i]);
-				FileUtils.copyInputStreamToFile(srcStream, destFile);
-			}
+			InputStream srcStream = ClassLoader.getSystemResourceAsStream("report/" + REPORT_RESOURCE);
+			File destFile = new File(dir, REPORT_RESOURCE);
+			FileUtils.copyInputStreamToFile(srcStream, destFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/main/resources/templates/report-template.ftl
+++ src/main/resources/templates/report-template.ftl
@@ -11,7 +11,6 @@
   href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
   integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"
   crossorigin="anonymous">
-<link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
