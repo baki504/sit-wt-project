d4fa526bc87e3b387b703862d5d633ed02e48d34
baki504 <baki504@msn.com>
2017-11-25 07:50:01 +0900

    古いレポートの削除機能実装

--- src/main/java/a/b/c/domain/report/ReportWriter.java
+++ src/main/java/a/b/c/domain/report/ReportWriter.java
@@ -1,9 +1,12 @@
 package a.b.c.domain.report;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 import a.b.c.domain.testscript.TestScript;
+import a.b.c.infra.ReportGenerationException;
 import a.b.c.infra.template.TemplateEngine;
 import a.b.c.infra.template.freemarker.TemplateEngineFreeMakerImpl;
 
@@ -19,9 +22,20 @@ public class ReportWriter {
 			return;
 		}
 
+		removeOld(reportFile);
+
 		Report report = buildReport(testScripts, reportFile);
 
 		templateEngine.write(report);
+
+	}
+
+	private void removeOld(Path reportFile) {
+		try {
+			Files.deleteIfExists(reportFile);
+		} catch (IOException e) {
+			throw new ReportGenerationException(e);
+		}
 
 	}
 
